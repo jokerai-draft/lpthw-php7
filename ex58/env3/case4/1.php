c8802c7a1a85c3128314031dee939723763f3e8c
noname <you@example.com>
2021-08-16 02:33:29 +0800

    update

--- ex58/env3/case4/1.php
+++ ex58/env3/case4/1.php
@@ -5,4 +5,4 @@ https://stackoverflow.com/questions/39748226/must-i-call-parent-construct-in-the
 
 ex40/proj2/ComputerPlayer.php
 
-不需要工厂模式 即使用到了 extendee 的 constructor 直接用即可
+不需要工厂模式 即使用到了 extendee 的 constructor 直接用 parent::__construct(); 即可
